37e89e0ef2cf8124eadec05ecedc561f82dbf0ad
Nishanth <nishanth@varmaandvarma.com>
2025-04-11 20:11:14 +0530

    Update personal_assisstant.py

--- personal_assisstant.py
+++ personal_assisstant.py
@@ -495,7 +495,7 @@ with tab2:
                     Format as a clear, bullet-point summary."""
                     
                     response = model.generate_content(prompt)
-                    st.container(response.text)
+                    st.container("response.text")
                     
                     # Additional analysis for common tags
                     if tags:
